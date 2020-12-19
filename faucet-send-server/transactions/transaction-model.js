d987f5a4edfe6edcd1e28d78d6bf43140f895e73
Michael Harms <michael.harms6010@gmail.com>
2020-12-19 13:29:58 -0600

    handles undef

--- faucet-send-server/transactions/transaction-model.js
+++ faucet-send-server/transactions/transaction-model.js
@@ -9,8 +9,6 @@ module.exports = {
 async function canGetTx(ip) {
     const timeThreshold = Math.floor(Date.now() / 1000) - (60 * 60 * 1)
     const txs = await db("transactions").where("ip", "=", ip).orderBy("datetime", "desc")
-    console.log("timeThreshold", timeThreshold)
-    console.log("txs[0].datetime", +txs[0].datetime > timeThreshold)
     if (!txs[0]) return true
     return  !(+txs[0].datetime > timeThreshold)
 

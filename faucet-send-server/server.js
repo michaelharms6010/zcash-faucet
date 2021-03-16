89b6848433ed4cd6df2d5cb694a09d1347e8dbc3
Michael Harms <michael.harms6010@gmail.com>
2021-03-16 18:41:52 -0500

    use testnet url

--- faucet-send-server/server.js
+++ faucet-send-server/server.js
@@ -32,62 +32,12 @@ server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
 
-async function sendZcash(zaddr, amount) {
-    let r;
-    try {
-        r = await axios({
-            method: 'post',
-            // url: "http://localhost:8232",
-            url: "http://localhost:18232",
-            headers: {
-                "Authorization": creds, 
-                'content-type': "application/json"
-            }, 
-            data: {
-                "jsonrpc": "1.0",
-                "id":"curltest", 
-                "method": "z_sendmany", 
-                "params": [process.env.MASTER_ZADDR, [{"address": zaddr ,"amount": amount }]] 
-            }
-        })
-        console.log(r)
-        return r
-    } catch (err) {
-        console.log(err.response.data.error)
-    }
-}
-
-async function getStatus(opid) {
-    let r;
-    try {
-        r = await axios({
-            method: 'post',
-            // url: "http://localhost:8232",
-            url: "http://localhost:18232",
-            headers: {
-                "Authorization": creds, 
-                'content-type': "application/json"
-            }, 
-            data: {
-                "jsonrpc": "1.0",
-                "id":"curltest", 
-                "method": "z_getoperationstatus", 
-                "params": [[opid]] 
-            }
-        })
-        console.log(r.data.result.status)
-        return r
-    } catch (err) {
-        console.log(err.response.data.error)
-    }
-}
-
 
 server.post("/sendtaz", async (req,res) => {
     let zaddr = req.body.address;
     let time = req.body.time;
     const ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-
+    
      if (await canGetTx(ip, zaddr)) {
         saveTx(zaddr, ip).then(r => {
             lightWallet.sendFaucet(zaddr, time)

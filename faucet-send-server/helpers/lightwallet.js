19ed7716377f1b2763b1238b3509422b2c776bd0
Michael Harms <michael.harms6010@gmail.com>
2021-03-16 17:38:53 -0500

    lightwallet mvp

--- faucet-send-server/helpers/lightwallet.js
+++ faucet-send-server/helpers/lightwallet.js
@@ -3,10 +3,40 @@ const {exec} = require("child_process");
 const db = require("../data/db-config");
 const Txns = require("../users/transactions/transactions-model");
 
+const Pusher = require("pusher");
+
+
+// todos - shuffle and env pusher creds
+// move model / endpoint functions to helper file
+
+const pusher = new Pusher({
+  appId: "1126204",
+  key: "4e18f1b8741914d03145",
+  secret: "3d2e94c5b0a7d3af6e76",
+  cluster: "us2",
+  useTLS: true
+});
+
 
 module.exports = {
     getNewDeposits,
+	sendFaucet
 }
+
+function sendFaucet(zaddr, time) {
+	exec(`./zecwallet-cli send 1000000 ${zaddr}`, (err, stdout, stderr) => {
+		console.log(err)
+		console.log(stderr)
+		console.log(stdout)
+		stdout = JSON.parse(stdout)
+		var ip = "0.0.0.0"
+
+		pusher.trigger("tx-notif", time, {
+				txid: stdout.txid
+		});
+	})
+}
+
 
 function getNewDeposits() {
     exec("./zecwallet-cli list", (err, stdout, stderr) => {
--- faucet-send-server/server.js
+++ faucet-send-server/server.js
@@ -7,6 +7,7 @@ const rpcCreds = process.env.ZCASH_RPC_CREDS;
 const creds = 'Basic ' + Buffer.from(rpcCreds).toString('base64').trim()
 const {canGetTx, saveTx, addTxId} = require("./transactions/transaction-model")
 const sleep = require("./helpers/sleep")
+const lightwallet = require("./helpers/lightwallet")
 
 const server = express();
 
@@ -84,48 +85,11 @@ async function getStatus(opid) {
 
 server.post("/sendtaz", async (req,res) => {
     let zaddr = req.body.address;
+    let time = req.body.time;
     console.log(req)
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
-    let amount = (Math.random() / 4 + 0.0001 ).toFixed(8) 
-    if (await canGetTx(ip, zaddr)) {
-        sendZcash(zaddr, amount)
-            .then(r => {
-                const opid = r.data.result;
-                saveTx(zaddr, ip, opid, amount).then(async r => {
-                    let txComplete = false;
-                    let result;
-                    res.status(200).json({message: "Sending", opid })
-                    while (!txComplete) {
-                        let status = await getStatus(opid);
-                        console.log(status.data.result)
-                        await sleep(2000)
-                        if (status.data.result[0].status != "executing") {
-                            txComplete = true;
-                            result = status.data.result[0]
-                        }
-                    }
-                    if (result.status == "success") {
-                        await addTxId(opid, result.result.txid)
-                        pusher.trigger("tx-notif", opid, {
-                            txid: result.result.txid
-                        });
-                        res.status(200).json({message: "Success", txid: result.result.txid })
-                    } else {
-                        pusher.trigger("tx-notif", opid, {
-                            error: "Zcashd failed the operation. Try again in a minute."
-                        });
-                        res.status(200).json({message: "success"})
-                    }
-                }).catch(err => res.status(500))
-            })
-            .catch(err => {
-                console.log(err)
-                `echo ${err} >> error.log`
-                res.status(500).json({message: "failed"})})
-    } else {
-        res.status(400).json({err: "You can only tap the faucet once every 15 minutes."})
-    }
+    lightwallet.sendFaucet(zaddr, time)
     
 })
 

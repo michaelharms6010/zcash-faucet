3b6292c194bdd1c6a1b27cb8a6c88fd6ff27c259
Michael Harms <michael.harms6010@gmail.com>
2021-04-17 18:15:53 -0500

    add generic rpc func

--- faucet-send-server/helpers/rpc.js
+++ faucet-send-server/helpers/rpc.js
@@ -1,8 +1,36 @@
 const axios = require("axios")
+const rpcCreds = process.env.ZCASH_RPC_CREDS;
+const creds = 'Basic ' + Buffer.from(rpcCreds).toString('base64').trim();
 
 module.exports = {
     sendZcash,
-    getStatus
+    getStatus,
+    doRPC
+}
+
+async function doRPC(method, params) {
+    let r;
+    try {
+        r = await axios({
+            method: 'post',
+            // url: "http://localhost:8232",
+            url: "http://localhost:18232",
+            headers: {
+                "Authorization": creds,
+                'content-type': "application/json"
+            },
+            data: {
+                "jsonrpc": "1.0",
+                "id":"curltest",
+                "method": method,
+                "params": params
+            }
+        })
+        console.log(r)
+        return r
+    } catch (err) {
+        console.log(err.response.data.error)
+    }
 }
 
 async function sendZcash(zaddr, amount) {
--- faucet-send-server/server.js
+++ faucet-send-server/server.js
@@ -2,15 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const axios = require("axios")
-const rpcCreds = process.env.ZCASH_RPC_CREDS;
-const creds = 'Basic ' + Buffer.from(rpcCreds).toString('base64').trim();
 const {canGetTx, saveTx, addTxId} = require("./transactions/transaction-model");
 const sleep = require("./helpers/sleep");
 const {sendZcash, getStatus} = require("./helpers/rpc");
-
-const TESTING_ZADDR = "ztestsapling18ul4pykvaglhjtfvgad7prgsks8fnx906xtjmq6vx3p8njqpurwhsndvf06yvw09ct7cwandp7w"
-
 
 
 const server = express();
